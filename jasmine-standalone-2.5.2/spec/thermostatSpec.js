afee31c2371d257fb55d2c8c07e0863613c0b548
Dines Poulsen <dinespoulsen84@gmail.com>
2016-12-07 12:08:26 +0000

    Added temp validations

--- jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -19,4 +19,26 @@ describe('thermostat', function() {
     thermostat.decreaseTemp(1);
     expect(thermostat.temperature).toEqual(19)
   });
+
+  it("should not be able to go below 10", function(){
+    expect( function(){thermostat.decreaseTemp(11)}).toThrow(new Error("Can't lower temperature: trying to go below minimum"))
+  });
+
+  it("should not be able to go above 25 when powermode is on", function(){
+    expect( function(){thermostat.increaseTemp(6)}).toThrow(new Error("Can't increase temperature: trying to go above maximum"))
+  });
+
+  it("should have a default power mode to on", function(){
+    expect(thermostat.powerMode).toBe(true)
+  });
+
+  it("should be able to change powermode", function(){
+    thermostat.powerToggle();
+    expect(thermostat.powerMode).toBe(false)
+  });
+
+  it("should throw an error when increasing temp above power mode maximum temp", function(){
+    thermostat.powerToggle();
+    expect( function(){thermostat.increaseTemp(13)}).toThrow(new Error("Can't increase temperature: trying to go above maximum"))
+  });
 });
--- jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine-standalone-2.5.2/src/thermostat.js
@@ -1,11 +1,38 @@
+const DEFAULT_TEMP = 20;
+const MINIMUM_TEMP = 10;
+const MAXIMUM_TEMP = 32;
+const POWERMODE_TEMP = 25;
+
 function Thermostat() {
-  this.temperature = 20;
+  this.temperature = DEFAULT_TEMP;
+  this.powerMode = true;
 }
 
   Thermostat.prototype.increaseTemp = function(temp) {
-    this.temperature += temp;
+    if(this.powerMode){
+      var maxTemp = POWERMODE_TEMP;
+    }
+    else {
+      var maxTemp = MAXIMUM_TEMP;
+    }
+
+    if(this.temperature + temp > maxTemp) {
+      throw Error("Can't increase temperature: trying to go above maximum")
+    }
+    else {
+      this.temperature += temp;
+    }
   };
 
   Thermostat.prototype.decreaseTemp = function(temp) {
-    this.temperature -= temp;
+    if(this.temperature - temp < MINIMUM_TEMP ) {
+      throw Error("Can't lower temperature: trying to go below minimum");
+    }
+    else {
+      this.temperature -= temp;
+    }
+  };
+
+  Thermostat.prototype.powerToggle = function() {
+    this.powerMode = !this.powerMode;
   };
