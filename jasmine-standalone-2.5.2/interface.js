7cb8aad29d1c05b9e68048ef8b7ec072e27008c8
pflarcology <rhrg500@gmail.com>
2016-12-07 17:22:59 +0000

    adds usage to html

--- jasmine-standalone-2.5.2/interface.js
+++ jasmine-standalone-2.5.2/interface.js
@@ -3,9 +3,14 @@ var minus = $("#minus");
 var thermostat = new Thermostat;
 var plus = $("#plus");
 var reset = $("#reset");
+var powermode = $("#powermode");
+var toggle = $("#powertoggle");
+var energyusage = $("#energyusage");
 
 window.onload = function() {
   currentTemp.text(thermostat.getCurrentTemperature());
+  powermode.text("on");
+  energyusage.text("medium-usage");
 };
 
 minus.click( function(){
@@ -20,4 +25,16 @@ plus.click( function(){
 reset.click( function(){
   thermostat.reset();
   currentTemp.text(thermostat.getCurrentTemperature());
+});
+
+toggle.click( function() {
+  thermostat.powerToggle();
+  powermode.text("off");
+  if(thermostat.powerMode) {
+    powermode.text("on");
+    currentTemp.text(thermostat.getCurrentTemperature());
+  }
+  else{
+    powermode.text("off");
+  }
 });
