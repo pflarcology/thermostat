740c3150428d9587350119cce81a6f5bc64bfc61
pflarcology <rhrg500@gmail.com>
2016-12-07 15:58:32 +0000

    basic jquery

---
+++ jasmine-standalone-2.5.2/testing.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $( "#minus").click(function( event ) {
+    alert( "the buttons don't acutally work, dines is everywhere")
+  });
+  $( "#minus" ).click(function( event ) {
+    $( this ).hide( "slow");
+  });
+});
