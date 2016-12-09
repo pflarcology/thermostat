33f0c344417cd138a253a2e22290d6fe98829a09
pflarcology <rhrg500@gmail.com>
2016-12-09 09:58:54 +0000

    adds degrees symbols

--- src/interface.js
+++ src/interface.js
@@ -9,17 +9,36 @@ var energyusage = $('#energyusage');
 
 $( document ).ready(function() {
   currenttemp.text(thermostat.showTemperature());
+
   energyusage.text(thermostat.energyUsage());
+
   powersavestatus.text(thermostat.showPowerSaveMode());
+
   $.get('http://api.openweathermap.org/data/2.5/weather?id=2646458&appid=669388914f66619c340185cc202f2758&units=metric', function(data) {
   $('#current-temperature').text(data.main.temp);
   });
+
+  // $('#current-city').change(function() {
+  //   var city = $('#current-city').val();
+  //   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=669388914f66619c340185cc202f2758&units=metric', function(data) {
+  //     $('#current-temperature').text(data.main.temp)
+  //   })
+  // })
+
   $('#current-city').change(function() {
-    var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=669388914f66619c340185cc202f2758&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp)
-    })
+    event.preventDefault();
+    var city = $("#current-city").val();
+      displayWeather(city);
+  });
+
+  function displayWeather(city) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city
+  var token = "&appid=669388914f66619c340185cc202f2758"
+  var units = "&units=metric"
+  $.get(url + token + units, function(data) {
+    $("#current-temperature").text(data.main.temp);
   })
+}
 
 
 
